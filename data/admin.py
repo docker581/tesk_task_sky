811780ba5109c153729330e02a3f09dd294c1e28
Denis Doktorov <denisd581@gmail.com>
2021-08-03 02:04:05 +0300

    Change data.admin.py

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient, Case, Document, Body
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['fio', 'birth_date', 'gender', 'id']
+
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'date_begin', 'date_end', 'result', 'id']
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'case', 'title', 'date', 'id']
+
+
+class BodyAdmin(admin.ModelAdmin):
+    list_display = ['document', 'content']
+
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Case, CaseAdmin)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Body, BodyAdmin)
